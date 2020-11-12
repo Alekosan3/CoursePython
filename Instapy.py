937080e2d52d5cf5a4eb9865af619c8ba1fc77ae
alekosansey <alekosansey3@gmail.com>
2020-11-12 17:20:56 +0600

    Class_SerchRelation

---
+++ Instapy.py
@@ -0,0 +1,39 @@
+
+class SearchRelationOfClass:
+    def __init__(self, n):
+        self.dictor = {}
+        for i in range(n):
+            l = input().split(':')
+            if len(l) > 1:
+                self.dictor.update({l[0]:l[1].split(' ')})
+            else:
+                self.dictor.update({l[0]:[]})
+
+    def print_all_list(self):
+        print(self.dictor.items())
+
+    def find_path(graph, start, end, path=[]):
+
+        path = path + [start]
+
+        if start == end:
+
+            return path
+
+        if not start in graph:
+
+            return None
+
+        for node in graph[start]:
+
+            if node not in path:
+
+                newpath = find_path(graph, node, end, path)
+
+                if newpath:
+                    return newpath
+
+        return None
+
+searcher = SearchRelationOfClass(5)
+searcher.print_all_list()
