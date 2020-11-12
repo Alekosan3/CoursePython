e11a2bbb90fef1c8c5072c277eef61b57e543e10
alekosansey3@gmail.com <16091987Alex>
2020-11-12 19:32:44 +0300

    Добавлены комментарии и убраны пробелы при добавлении значений в словарь

--- Instapy.py
+++ Instapy.py
@@ -2,23 +2,26 @@
 class SearchRelationOfClass:
     def __init__(self, n):
 
-        self.dictor = {}
+        self.dictor = {} # Создаем словарь чтобы туда поместить Граф. Значения которые мы потом будем перебирать
 
         for i in range(n):
 
-            l = input().split(':')
+            l = input().split(':') # разбиваем введенное значение на ключ и значение. Значением будет список
+            l = [el.strip() for el in l] # убираем лишние пробелы справа и слева чтобы не получить левые данные
 
-            if len(l) > 1:
+
+            if len(l) > 1: # Если список состоит из двух элементов то добавляем в словарь ключ и список со значениями
 
                 self.dictor.update({l[0]:l[1].split(' ')})
+                print(self.dictor)
 
-            else:
+            else: # Иначе если список состоит только с одним значением то создаем ключ с пустым списком
 
                 self.dictor.update({l[0]:[]})
 
     def print_all_list(self):
 
-        print(self.dictor.items())
+        print(self.dictor.items()) # Выводим все что есть в словаре чтобы проверить правильно ли попадают значения туда
 
     def find_path(graph, start, end, path=[]):
 
