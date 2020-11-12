6503f387ea0dbd85a7740a24cc967413d7393d80
alekosansey <alekosansey3@gmail.com>
2020-11-12 17:24:13 +0600

    Change_space_in_program

--- Instapy.py
+++ Instapy.py
@@ -1,12 +1,19 @@
 
 class SearchRelationOfClass:
     def __init__(self, n):
+
         self.dictor = {}
+
         for i in range(n):
+
             l = input().split(':')
+
             if len(l) > 1:
+
                 self.dictor.update({l[0]:l[1].split(' ')})
+
             else:
+
                 self.dictor.update({l[0]:[]})
 
     def print_all_list(self):
@@ -36,4 +43,5 @@ class SearchRelationOfClass:
         return None
 
 searcher = SearchRelationOfClass(5)
+
 searcher.print_all_list()
